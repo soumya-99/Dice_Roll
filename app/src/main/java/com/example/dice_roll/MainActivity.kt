3eef390cd85174c75cf7ca124cf35a4205b1dfe0
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-05-03 17:30:56 +0530

    first commit

---
+++ app/src/main/java/com/example/dice_roll/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.dice_roll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnClick(view: View) {
+        val randomNumber = (1..6).random()
+        textView.text = randomNumber.toString()
+    }
+}
